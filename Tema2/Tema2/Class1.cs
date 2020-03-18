18570f9d434bcb86dfd68ea690870a131dc23eb2
Andrei Ventuneac <andreiventuneac@hotmail.com>
2020-03-18 11:48:50 +0200

    Tema2 PIU

---
+++ Tema2/Tema2/Class1.cs
@@ -0,0 +1,93 @@
+﻿//Tema2 Lab3 ----- Ex. 1
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Teema1
+{
+    class Class1
+    {
+        double nota;
+        string nume, prenume;
+        string status;
+
+        public Class1()
+        {
+            nota = 0;
+            nume = string.Empty;
+            prenume = string.Empty;
+            status = string.Empty;
+        }
+
+
+        public Class1(string nume_, string prenume_, double nota_)
+        {
+            nota = nota_;
+            nume = nume_;
+            prenume = prenume_;
+        }
+
+        //adaugare constructor nou ce primeste un sir de caractere - lab3
+        public Class1(string text)
+        {
+            int k = 0;
+            string[] cuvinte = text.Split(", ");
+            foreach(string cuv in cuvinte)
+            {
+                if (k == 0)
+                    nume = cuv;
+                if (k == 1)
+                    prenume = cuv;
+                if (k == 2)
+                    nota = Convert.ToDouble(cuv);
+                k++;
+            }
+        }
+        //
+        public string afisare()
+        {
+            if (nota >= 5)
+                return string.Format("Elevul {0} {1} are nota {2} si este admis", nume, prenume, nota);
+            else
+                return string.Format("Elevul {0} {1} are nota {2} si este respins", nume, prenume, nota);
+        }
+
+        public string afisareresp()
+        {
+            if (nota < 5)
+                return string.Format("{0} {1} : nota {2}", nume, prenume, nota);
+            else
+                return string.Empty;
+        }
+
+        public string getnumepr()
+        {
+            return string.Format("{0} {1}", nume, prenume);
+        }
+        public string reexaminare()
+        {
+            return string.Empty;
+        }
+
+        public void setnota(double nota_)
+        {
+            nota = nota_;
+        }
+
+        public void setstatus(double nota)
+        {
+            if (nota >= 5)
+                status = "admis";
+            else
+                status = "respins";
+        }
+
+        public string getstatus()
+        {
+            return status;
+        }
+
+
+    }
+}
---
+++ Tema2/Tema2/Program.cs
@@ -0,0 +1,115 @@
+﻿//Tema2 Lab3 ----- Ex. 1
+
+using System;
+
+namespace Teema1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tema1 PIU Andrei Ventuneac 3121A Calculatoare\n");
+
+
+            //APELARE CONSTRUCTOR NOU LAB3---------
+            Class1 elev1 = new Class1("Ion, Popescu, 9.5");
+            Console.WriteLine(elev1.afisare());
+
+            //test primul elev + afisare
+            Class1 elev = new Class1("Andrei", "Tudor", 10);
+            Console.WriteLine(elev.afisare());
+
+            //sir de obiecte + functie random
+            string[] nume = new string[] { "Popescu", "Maradona", "Georgescu", "Scutescu" };
+            string[] prenume = new string[] { "Andrei", "Alexandru", "Mircea", "Stefan" };
+
+            Class1[] elevi = new Class1[50];
+            Random rnd = new Random();
+            for (int i = 0; i <= 10; i++)
+            {
+                string nume_ = nume[rnd.Next(0, nume.Length)];
+                string prenume_ = prenume[rnd.Next(0, prenume.Length)];
+                double nota_ = rnd.Next(1, 10);
+                elevi[i] = new Class1(nume_, prenume_, nota_);
+                elevi[i].setstatus(nota_);
+                Console.WriteLine(elevi[i].afisare());
+            }
+
+            //afisare elevi respinsi
+            int nr = 0;
+            Console.WriteLine("\nElevii respinsi:");
+            for (int i = 0; i <= 10; i++)
+            {
+                if (elevi[i].afisareresp() != string.Empty)
+                {
+                    nr++;
+                    Console.WriteLine(elevi[i].afisareresp());
+                }
+            }
+
+            //reexaminare - raspunde la o intrebare pentru a trece sau ramai picat!
+            Console.WriteLine("\nDoriti o reexaminare a elevilor? (DA/NU)");
+            string raspuns;
+            raspuns = Console.ReadLine();
+            string[] intrebare = new string[] { "Ce functie este utilizata pentru a afisa un text pe o line de consola?", "Ce functie este utilizata pentru a citi o linie de pe consola?", "O clasa poate avea membrii publici?" };
+            string[] raspunss = new string[] { "Console.WriteLine()", "Console.ReadLine()", "DA" };
+            int admis = 0;
+
+            if (raspuns == "DA")
+            {
+                for (int i = 0; i <= 10; i++)
+                {
+                    if (elevi[i].getstatus().Equals("respins"))
+                    {
+                        Console.Write(elevi[i].getnumepr() + " cu intrebarea:  ");
+                        string intrebare_ = intrebare[rnd.Next(0, intrebare.Length)];
+                        Console.Write(intrebare_ + "\n");
+                        string raspuns_ = Console.ReadLine();
+
+                        for (int j = 0; j <= 2; j++)
+                        {
+                            if (intrebare_.Equals(intrebare[j]))
+                            {
+                                if (raspuns_.Equals(raspunss[j]))
+                                {
+                                    admis = 1;
+                                }
+                                else
+                                {
+                                    admis = 0;
+                                }
+                            }
+                        }
+
+                        if (admis == 1)
+                        {
+                            Console.WriteLine("Felicitari sunteti admisi cu nota 5!\n");
+                            elevi[i].setnota(5);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ati picat iar testul!");
+                        }
+
+                    }
+                }
+            }
+
+            //reafisare elevi respinsi
+
+            for (int i = 0; i <= 10; i++)
+            {
+                if (elevi[i].getstatus().Equals("respins"))
+                {
+                    Console.WriteLine(elevi[i].afisareresp());
+                }
+            }
+
+
+
+
+
+
+        }
+    }
+}
