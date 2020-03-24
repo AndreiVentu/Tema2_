b16fbcfb5b6722d997562c8c877c1a255ee5e727
Andrei Ventuneac <andreiventuneac@hotmail.com>
2020-03-24 18:07:57 +0200

    Ex3

---
+++ Tema2Ex3/Tema2Ex3/Program.cs
@@ -0,0 +1,115 @@
+﻿using LibrarieEntitati;
+using System;
+
+
+namespace ManagementStudenti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tema2 PIU Andrei Ventuneac 3121A");
+            Student s = new Student("Ionescu");
+            char c;
+            int n, nr = 0,nran=1;
+            int[,] _note = new int[4,15];
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("S: Citire note de la tastatura ca un sir de caractere");
+                Console.WriteLine("T: Citire note de la tastatura (cate o nota pe rand)");
+                Console.WriteLine("C: Citire note din linia de comanda");
+                Console.WriteLine("A: Afiaare note");
+                Console.WriteLine("I: Info autor");
+                Console.WriteLine("N: Afisare numar note <5 & >5");
+                Console.WriteLine("M: MATRICE");
+                Console.WriteLine("X: Iesire");
+
+                Console.Write("\nAlege optiunea: ");
+                c = Convert.ToChar(Console.ReadLine());
+                Console.Write("\n");
+
+                switch (c)
+                {
+                    case 'S':
+                        Console.Write("CITIRE SIR: ");
+                        s.refresh();
+                        string note = s.Citiretastsir();
+                        s.SetNote(note);
+                        Console.WriteLine(s.ConversieLaSir());
+                        break;
+
+                        case 'T':
+                        s.refresh();
+                        for (int i = 0; i < 4; i++)
+                        {
+                            Console.Write("Introduceti numarul de note pentru anul" + nran + ": ");
+                            nran++;
+                            n = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Citire note: ");
+                            for (int j= 0; j < n; j++)
+                            {
+                               _note[i,j] = Convert.ToInt32(Console.ReadLine());
+                            }
+                        }
+                            s.SetNote(_note);
+                            Console.WriteLine(s.ConversieLaSir());
+                            break;
+                        
+                        case 'C':
+                        s.refresh();
+                        string txt = string.Join(" ",args);
+                            //Console.WriteLine(txt);
+                            s.SetNote(txt);
+                            Console.WriteLine(s.ConversieLaSir());
+                            break;
+
+                        case 'A':
+                            Console.WriteLine(s.ConversieLaSir());
+                            break;
+
+                        case 'N':
+                            int mic, mare;
+                            s.NumarareNote(out mic, out mare);
+                            Console.WriteLine("NUMAR NOTE MAI MICI DE 5 sunt :" + mic);
+                            Console.WriteLine("NUMAR NOTE MAI MARI DE 5 sunt: " + mare);
+                            break;
+
+                        case 'I':
+                            Console.WriteLine("ANDREI VENTUNEAC CONSOLEKILLER 3121A CALCULATOARE 2020 TO BE CONTINUED!");
+                            break;
+
+                        case 'X':
+                            Environment.Exit(1);
+                            break;
+
+                        case 'M':
+                            int[,] mat = new int[10,15];
+                            for(int i =0;i<=5;i++)
+                            {
+                                for(int j = 0;j<=5;j++)
+                                {
+                                    mat[i,j] = j;
+                                }
+                            }
+
+                            for (int i = 0; i <= 5; i++)
+                            {
+                                for (int j = 0; j <= 5; j++)
+                                {
+                                    Console.Write(mat[i, j]);
+                                }
+                                Console.Write("\n");
+                            }
+
+                            break;
+    
+                }
+
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ Tema2Ex3/Tema2Ex3/Student.cs
@@ -0,0 +1,133 @@
+﻿namespace LibrarieEntitati
+{
+    public class Student
+    {
+        // date membre private
+        string nume;
+        int[,] note = new int[4,15];
+        const int MINIM = 1;
+        const int MAXIM = 10;
+        const int MIN = 5;
+
+        // contructor implicit
+        public Student()
+        {
+            
+        }
+
+        // constructor cu parametri
+        public Student(string _nume)
+        {
+            nume = _nume;
+        }
+        public string Citiretastsir()
+        {
+            string text;
+            text = System.Console.ReadLine();
+            return text;
+        }
+
+        // reinitializare matrice la aplearea fiecarei citiri
+        public void refresh()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                {
+                    note[i, j] = 0;
+                }
+            }
+        }
+
+        public void SetNote(int[,] _note)
+        {
+            int nr = 0, k;
+           
+            for (int i = 0; i < 4; i++)
+            {
+                k = 0;
+                for(int j = 0;j<15;j++)
+                {
+                    if (_note[i,j] >= MINIM && _note[i,j] <= MAXIM)
+                    {
+                        note[i,k] = _note[i,k];
+                        k++;
+                    }
+                }
+               
+            }
+        }
+
+   
+        public void SetNote(string sirNote)
+        {
+            int k = 0, i, j = 0,bun;
+
+
+            string[] _note = sirNote.Split(',');
+            string[] note_;
+            
+            for (i = 0; i < _note.Length; i++)
+            {
+                k = 0;
+                note_ = _note[i].Split(' ');
+                foreach (string nota in note_)
+                {
+                    if ((int.TryParse(nota, out bun)) && bun >= MINIM && bun <= MAXIM)
+                    {
+                        note[i, k] = bun;
+                        k++;
+                    }
+                }
+            }
+
+        }
+    
+
+        public string ConversieLaSir()
+        {
+            int k = 0;
+            string sNote ="";
+            if(note!=null)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    sNote = sNote + "Anul " + (i+1) +": ";
+                    for (int j = 0; j < 15; j++)
+                    {
+                        sNote = sNote + System.Convert.ToString(note[i, j])+" ";
+                    }
+                    sNote = sNote + "\n";
+                }
+            }
+            string s = string.Format("Studentul {0} are notele:.... \n{1}", nume, sNote);
+
+            return s;
+        }
+
+        
+        public void NumarareNote(out int valSub5, out int valPeste5)
+        {
+            valSub5 = valPeste5 = 0;
+            if (note is null)
+            {
+
+            }
+            else
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 15; j++)
+                    {
+                        if (note[i, j] < MIN && note[i, j]!=0)
+                            valSub5++;
+                        if (note[i, j] > MIN )
+                            valPeste5++;
+                    }
+                }
+            }
+
+        }
+    }
+    
+}
